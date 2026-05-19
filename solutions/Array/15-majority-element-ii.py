04d5aa5e8c0eb3d5a309d2cf47340b238b81f2e8
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2026-05-19 11:32:57 +0530

    Add solution: Majority Element II

--- solutions/Array/15-majority-element-ii.py
+++ solutions/Array/15-majority-element-ii.py
@@ -1,15 +1,1 @@
-# tc = O(n)
-# sc = O(1)
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        count = 1
-        cur = nums[0]
-        for i in range(1, len(nums)):
-            if nums[i] == cur:
-                count += 1
-                continue
-            count -= 1
-            if count == 0:
-                cur = nums[i]
-                count = 1
-        return cur
+na
