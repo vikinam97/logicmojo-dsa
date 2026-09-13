54a54d532fa491cc093b1279d51f0b16a838cbe0
vikinam97 <41544087+vikinam97@users.noreply.github.com>
2026-09-13 15:41:46 +0530

    Add solution: Remove Duplicates from Sorted List

---
+++ solutions/Linked List/42-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+        while node:
+            t = node.next
+            while t and node.val == t.val:
+                t = t.next
+
+            node.next = t
+            node = t
+
+        return head 
+        
